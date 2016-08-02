2cda9a87c9baa00cb6e18ae4ec847330e697a497
beroid <beroid3@gmail.com>
2016-08-02 10:13:16 +0500

    crackling: Enable B service adj transition by default

--- product/misc.mk
+++ product/misc.mk
@@ -5,3 +5,9 @@ PRODUCT_PACKAGES += \
 # Stlport
 PRODUCT_PACKAGES += \
     libstlport
+
+#Enable B service adj transition by default
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bservice_enable=true \
+    ro.sys.fw.bservice_limit=5 \
+    ro.sys.fw.bservice_age=5000
