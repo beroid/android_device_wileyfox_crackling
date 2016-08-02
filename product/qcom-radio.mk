f45d62d41a905c3cab414bc43516b296bda4509d
beroid <beroid3@gmail.com>
2016-08-02 10:09:41 +0500

    crackling: Update radio props

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -5,8 +5,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.ecc_hard_1=998 \
     persist.radio.ecc_hard_count=1 \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
-    ro.telephony.default_network=9,9 \
-    ro.ril.multi_rat_capable=true
+    ro.telephony.default_network=9,9
 
 # RIL
 ifeq ($(QCPATH),)
