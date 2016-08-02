e30e4074d4336c8176a1e340fbb4db7dcbf636fe
beroid <beroid3@gmail.com>
2016-08-02 10:13:16 +0500

    crackling: Enable B service adj transition by default

--- product/misc.mk
+++ product/misc.mk
@@ -1,3 +1,9 @@
 # CMActions
 PRODUCT_PACKAGES += \
     CMActions	
+
+#Enable B service adj transition by default
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bservice_enable=true \
+    ro.sys.fw.bservice_limit=5 \
+    ro.sys.fw.bservice_age=5000
